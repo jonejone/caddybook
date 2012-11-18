c3a303684668bca90cadeeff00bec3955a54fd5b
Jone Eide <jone@idev.no>
2012-11-18 12:19:43 +0100

    Added utility for generating courses

--- caddybook/books/tests.py
+++ caddybook/books/tests.py
@@ -1,16 +1,15 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from caddybook.books.models import Course, Hole
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+def generate_course(name, holes=18):
+    course = Course.objects.create(
+        name=name, active=True, slug=name.lower())
+
+    for i in range(holes):
+        n = i + 1
+        name = 'Hole %i' % n
+
+        Hole.objects.create(
+            course=course, position=n, name=name)
+
+    return course
