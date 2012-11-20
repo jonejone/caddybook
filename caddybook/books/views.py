53057f77e11fde8783771bd8f160b5bb79bb96aa
Jone Eide <jone@idev.no>
2012-11-20 10:36:09 +0100

    Added Edit Hole view

--- caddybook/books/views.py
+++ caddybook/books/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from caddybook.books.models import Course, Hole
+from caddybook.books.forms import HoleForm
 
 def index(request):
     courses = Course.objects.filter(active=True)
@@ -25,3 +28,26 @@ def hole(request, slug, position):
 
     return render(request, 'books/hole.html', {
         'hole': hole })
+
+
+def hole_edit(request, slug, position):
+    course = get_object_or_404(Course,
+        slug=slug, active=True)
+
+    hole = Hole.objects.get(course=course,
+        position=position)
+
+    if request.method == 'POST':
+        form = HoleForm(request.POST, request.FILES,
+            instance=hole)
+
+        if form.is_valid():
+            updated_hole = form.save()
+            return HttpResponseRedirect(reverse(
+                'books-hole', args=[hole.course.slug,
+                hole.position]))
+    else:
+        form = HoleForm(instance=hole)
+
+    return render(request, 'books/hole_edit.html', {
+        'hole': hole, 'form': form })
