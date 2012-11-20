1f4b6773ee642b8aac36f86842ac440949124d20
Jone Eide <jone@idev.no>
2012-11-20 10:35:28 +0100

    Small changes to settings

--- caddybook/books/urls.py
+++ caddybook/books/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,5 +8,13 @@ urlpatterns = patterns('caddybook.books.views',
     url(r'^courses/(?P<slug>[-\w]+)/$', 'course', name='books-course'),
     url(r'^courses/(?P<slug>[-\w]+)/holes/(?P<position>\d+)/$',
         'hole', name='books-hole'),
+    url(r'^courses/(?P<slug>[-\w]+)/holes/(?P<position>\d+)/edit/$',
+        'hole_edit', name='books-hole-edit'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                        'document_root': settings.MEDIA_ROOT,
+                        'show_indexes': True}))
