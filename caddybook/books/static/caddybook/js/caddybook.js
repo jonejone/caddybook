f3c879509fb31034a8d2e6ff5d6c4d0e70db5fc8
Jone Eide <jone@idev.no>
2012-11-21 01:35:12 +0100

    Added base caddybook scope

--- caddybook/books/static/caddybook/js/caddybook.js
+++ caddybook/books/static/caddybook/js/caddybook.js
@@ -14,3 +14,50 @@
     window.CADDYBOOK = CADDYBOOK;
 }(window, document));
 
+(function(caddybook) {
+
+    var AjaxStatusBar = function(container) {
+        this.container = container;
+        this.span = this.container.find('span');
+        this.type = 'warning';
+    };
+
+    AjaxStatusBar.prototype = {
+        setType: function(type) {
+            this.span.removeClass('label-' + this.type);
+            this.span.addClass('label-' + type);
+            this.type = type;
+        },
+
+        setMessage: function(message) {
+            this.span.html(message);
+        },
+
+        setSuccess: function(message) {
+            this.setType('success');
+            this.setMessage(message);
+            this.show();
+            this.container.delay(3000).fadeOut();
+        },
+
+        setWarning: function(message) {
+            this.setType('warning');
+            this.setMessage(message);
+            this.show();
+        },
+
+        hide: function() {
+            this.container.hide();
+        },
+
+        show: function() {
+            this.container.fadeIn();
+        },
+    };
+
+    $(function() {
+        var ajax_status_bar = new AjaxStatusBar($('.ajax-status-bar'));
+        caddybook.ajax_status_bar = ajax_status_bar;
+    });
+
+}(CADDYBOOK.module('caddybook')));
