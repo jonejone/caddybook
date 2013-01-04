4fb068015d109486f17483fb6850071fb378bb48
Jone Eide <jone@idev.no>
2013-01-04 01:41:42 +0100

    Added logging to stdout

--- caddybook/settings/default_settings.py
+++ caddybook/settings/default_settings.py
@@ -128,6 +128,8 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+import sys
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -141,7 +143,12 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'strm': sys.stdout
+        },
     },
     'loggers': {
         'django.request': {
