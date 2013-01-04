d7d23f0cf5aa951511dd77c1bdddc445928693c3
Jone Eide <jone@idev.no>
2013-01-04 01:58:51 +0100

    Added Sendgrid settings

--- caddybook/settings/caddybook_settings.py
+++ caddybook/settings/caddybook_settings.py
@@ -68,3 +68,9 @@ MEDIA_URL = '%smedia/' % AWS_BUCKET_URL
 ADMINS = (
     ('Jone Eide', 'jone@idev.no'),
 )
+
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
